cc0a28d606f4a9c632bad916c9ca68f4fddeaaf7
Manu <manu@noreply.git>
2018-04-28 12:09:01 +0200

    added cfgwnd

---
+++ Snek_Game/ConfigWindow.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Snek_Game
+{
+    partial class ConfigWindow
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_WndInfo = new System.Windows.Forms.Label();
+            this.btn_CloseWnd = new System.Windows.Forms.Button();
+            this.btn_loadcfg = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_WndInfo
+            // 
+            this.lbl_WndInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lbl_WndInfo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbl_WndInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_WndInfo.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lbl_WndInfo.Location = new System.Drawing.Point(0, 0);
+            this.lbl_WndInfo.Name = "lbl_WndInfo";
+            this.lbl_WndInfo.Size = new System.Drawing.Size(766, 27);
+            this.lbl_WndInfo.TabIndex = 0;
+            this.lbl_WndInfo.Text = this.Text;
+            this.lbl_WndInfo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lbl_WndInfo_MouseDown);
+            this.lbl_WndInfo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lbl_WndInfo_MouseMove);
+            this.lbl_WndInfo.MouseUp += new System.Windows.Forms.MouseEventHandler(this.lbl_WndInfo_MouseUp);
+            // 
+            // btn_CloseWnd
+            // 
+            this.btn_CloseWnd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_CloseWnd.BackColor = System.Drawing.Color.Maroon;
+            this.btn_CloseWnd.FlatAppearance.BorderSize = 0;
+            this.btn_CloseWnd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_CloseWnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_CloseWnd.Location = new System.Drawing.Point(725, 0);
+            this.btn_CloseWnd.Name = "btn_CloseWnd";
+            this.btn_CloseWnd.Size = new System.Drawing.Size(36, 22);
+            this.btn_CloseWnd.TabIndex = 4;
+            this.btn_CloseWnd.Text = "X";
+            this.btn_CloseWnd.UseVisualStyleBackColor = false;
+            this.btn_CloseWnd.Click += new System.EventHandler(this.btn_CloseWnd_Click);
+            // 
+            // btn_loadcfg
+            // 
+            this.btn_loadcfg.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_loadcfg.BackColor = System.Drawing.Color.Maroon;
+            this.btn_loadcfg.FlatAppearance.BorderSize = 0;
+            this.btn_loadcfg.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_loadcfg.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_loadcfg.Location = new System.Drawing.Point(12, 451);
+            this.btn_loadcfg.Name = "btn_loadcfg";
+            this.btn_loadcfg.Size = new System.Drawing.Size(158, 35);
+            this.btn_loadcfg.TabIndex = 7;
+            this.btn_loadcfg.Text = "Load Configuration";
+            this.btn_loadcfg.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.BackColor = System.Drawing.Color.Maroon;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(176, 451);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 35);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Save Configuration";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.Maroon;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(596, 451);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 35);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Apply Configuration";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // ConfigWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.ClientSize = new System.Drawing.Size(766, 498);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_loadcfg);
+            this.Controls.Add(this.btn_CloseWnd);
+            this.Controls.Add(this.lbl_WndInfo);
+            this.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ConfigWindow";
+            this.Text = "Config";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_WndInfo;
+        private System.Windows.Forms.Button btn_CloseWnd;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_loadcfg;
+    }
+}
+
